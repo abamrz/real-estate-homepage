1dea2882648c9f531cd09fd624cdd5b4bdfd4b72
abamrz <mirzakhidov@mail.ru>
2019-12-10 09:48:22 +0100

    Add RealtorsAdmin to manage realtors page

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Realtor
+
 
 # Register your models here.
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nme', 'is_mvp', 'email')
+    list_filter = ('is_mvp',)
+    search_fields = ('name', 'is_mvp')
+
+
+admin.site.register(Realtor, RealtorAdmin)
