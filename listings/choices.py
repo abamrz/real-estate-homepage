446814f0a626b0f4f351ba9872e4d1612b37db1b
abamrz <mirzakhidov@mail.ru>
2019-12-13 22:55:05 +0100

    Create choice options for search

---
+++ listings/choices.py
@@ -0,0 +1,32 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10
+}
+
+price_choices = {
+    '100000': 'EUR 100000',
+    '200000': 'EUR 200000',
+    '300000': 'EUR 300000',
+    '400000': 'EUR 400000',
+    '500000': 'EUR 500000'
+}
+
+state_choices = {
+    'BW': 'Baden-Wurttemberg',
+    'BY': 'Bayern',
+    'BE': 'Berlin',
+    'HE': 'Hessen',
+    'SA': 'Sachsen-Anhalt',
+    'SN': 'Sachsen',
+    'NS': 'Niedersachsen',
+    'HH': 'Hamburg',
+    'SH': 'Schleswig-Holstein'
+}
