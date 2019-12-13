9173b48ba1feb8879fe77574d7520308284721da
abamrz <mirzakhidov@mail.ru>
2019-12-13 22:56:56 +0100

    Import choice options for search

--- real_estate_page/seiten/views.py
+++ real_estate_page/seiten/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, state_choices, bedroom_choices
 
 
 # Create your views here.
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     context = {
-        'listings': listings
+        'listings': listings,
+        'states': state_choices,
+        'price': price_choices,
+        'bedroom': bedroom_choices
     }
     return render(request, 'pages/index.html', context)
 
