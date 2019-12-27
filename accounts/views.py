5ffb33a81f9cb7ee7eb550a0999aa8333e33e8a4
abamrz <mirzakhidov@mail.ru>
2019-12-27 21:09:43 +0100

    feature: add user data sign up validation

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        # Register
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Password match validation
+        if password == password2:
+            # Username validation
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username is taken')
+                return redirect('register')
+            else:
+                # Email validation
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email exists')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password,
+                                                    email=email, first_name=first_name, last_name=last_name)
+                    user.save()
+                    # auth.login(request, user)
+                    messages.success(request, 'Registered successfully')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
